87d6f20f7d1f8ac876935a575e280ee5890e3161
Jwhyee <kawnsdud@naver.com>
2022-06-28 11:21:34 +0900

    헤드 파일 추가

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "../components/Head";
+
+function AboutPage() {
+  return (
+    <main>
+      <title>어바웃</title>
+      <Head />
+
+      <h1>어바웃 페이지 입니다.</h1>
+    </main>
+  );
+}
+
+export default AboutPage;
--- src/pages/test-space.js
+++ src/pages/test-space.js
@@ -1,16 +1,11 @@
-import { Link } from "gatsby";
 import React from "react";
+import Head from "../components/Head";
 
 function TestSpacePage() {
   return (
     <main>
       <title>테스트 스페이스</title>
-      <header style={{ display: "flex" }}>
-        <Link to="/" style={{ padding: 10 }}>로고</Link>
-        <div style={{ flexGrow: 1 }}></div>
-        <Link to="/test" style={{ padding: 10 }}>테스트</Link>
-        <Link to="/test-space" style={{ padding: 10 }}>테스트 스페이스</Link>
-      </header>
+      <Head />
 
       <h1>테스트 스페이스 페이지 입니다.</h1>
     </main>

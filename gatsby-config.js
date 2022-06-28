acc5eec18fb80ab175b8774f2129a0bd463ba072
Jwhyee <kawnsdud@naver.com>
2022-06-28 15:05:54 +0900

    config 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Jwhy's Blog`,
     siteUrl: `https://www.yourdomain.tld`,
+    description: "이 사이트에 개발자 가준영의 개발일지가 담겨있습니다.",
   },
   plugins: [
     "gatsby-plugin-image",

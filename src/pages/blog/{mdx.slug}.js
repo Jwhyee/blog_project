d1ca69761d73a28752de079314d946632d4ce023
Jwhyee <kawnsdud@naver.com>
2022-06-28 17:33:35 +0900

    블로그 완성

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import Layout from "../../components/layout";
+import Layout from "../../components/Layout";
 const BlogPost = ({ data }) => {
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}>
@@ -11,14 +11,14 @@ const BlogPost = ({ data }) => {
     );
 };
 export const query = graphql`
-    query($id:String){
-        mdx(id: { eq: $id }){
-            frontmatter{
-                title
-                date(formatString: "MMMM D, YYYY")
-            }
-            body
-        }
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
     }
+  }
 `;
 export default BlogPost;
